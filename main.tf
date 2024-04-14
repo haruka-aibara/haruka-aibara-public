ae05734327702958ec000d63dba6e8bb4ca94304
haruka-aibara <haruka_aibara@outlook.com>
2024-04-14 20:47:50 +0900

    VPC作成

--- main.tf
+++ main.tf
@@ -2,30 +2,30 @@
 # Terraform configuration
 # ------------------------
 terraform {
-    required_version = ">=0.13"
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = ">3.0"
-        }
+  required_version = ">=0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">3.0"
     }
+  }
 }
 
 # ------------------------
 # Provider
 # ------------------------
 provider "aws" {
-    profile = "default"
-    region = "ap-northeast-1"
+  profile = "default"
+  region  = "ap-northeast-1"
 }
 
 # ------------------------
 # Variables
 # ------------------------
 variable "project" {
-    type = string
+  type = string
 }
 
 variable "environment" {
-    type = string
+  type = string
 }
