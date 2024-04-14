d456a8a0a639584f75d9bfe7a70a500c212ba8ef
haruka-aibara <haruka_aibara@outlook.com>
2024-04-14 21:55:46 +0900

    パブリックサブネットのipv4自動割り当て漏れを修正

--- network.tf
+++ network.tf
@@ -22,7 +22,7 @@ resource "aws_subnet" "public_subnet_1a" {
   vpc_id                  = aws_vpc.vpc.id
   availability_zone       = "ap-northeast-1a"
   cidr_block              = "192.168.1.0/24"
-  map_public_ip_on_launch = false
+  map_public_ip_on_launch = true
 
   tags = {
     Name    = "${var.project}-${var.environment}-public-subnet-1a"
@@ -36,7 +36,7 @@ resource "aws_subnet" "public_subnet_1c" {
   vpc_id                  = aws_vpc.vpc.id
   availability_zone       = "ap-northeast-1c"
   cidr_block              = "192.168.2.0/24"
-  map_public_ip_on_launch = false
+  map_public_ip_on_launch = true
 
   tags = {
     Name    = "${var.project}-${var.environment}-public-subnet-1c"
