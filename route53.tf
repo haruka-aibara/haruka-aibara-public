6880cceed93eb502d470e65b99bfd4a7b11aa726
haruka-aibara <haruka_aibara@outlook.com>
2024-04-28 17:45:09 +0900

    Aレコード作成

--- route53.tf
+++ route53.tf
@@ -11,3 +11,14 @@ resource "aws_route53_zone" "route53_zone" {
     Env     = var.environment
   }
 }
+
+resource "aws_route53_record" "route53_record" {
+  name    = "dev-elb.${var.domain}"
+  type    = "A"
+  zone_id = aws_route53_zone.route53_zone.id
+  alias {
+    evaluate_target_health = true
+    name                   = aws_lb.alb.dns_name
+    zone_id                = aws_lb.alb.zone_id
+  }
+}
